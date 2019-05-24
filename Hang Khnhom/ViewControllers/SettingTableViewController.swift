13d717e900004669144ba167f34f080b344f303c
Vibol's Macbook Pro <vibolsmacbookpro@vibolss-macbook-pro.local>
2019-05-24 16:26:38 +0700

    - design setting screen

---
+++ Hang Khnhom/ViewControllers/SettingTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingTableViewController.swift
+//  Hang Khnhom
+//
+//  Created by Vibol's Macbook Pro on 5/24/19.
+//  Copyright © 2019 Vibol Roeun. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1 {
+            return 60
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3
+        }
+        
+        return 2
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2
+    }
+
+
+}
--- Hang Khnhom/ViewControllers/SettingViewController.swift
+++ Hang Khnhom/ViewControllers/SettingViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+    
+    
+    
     
 
 }
