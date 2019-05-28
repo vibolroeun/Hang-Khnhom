b85947cc727cf3558ac60ae0362cfcbfc1d9d1d1
Vibol's Macbook Pro <vibolsmacbookpro@vibolss-macbook-pro.local>
2019-05-28 22:03:25 +0700

    - design verification screen

---
+++ Hang Khnhom/ViewControllers/VierificationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  VierificationViewController.swift
+//  Hang Khnhom
+//
+//  Created by Vibol's Macbook Pro on 5/28/19.
+//  Copyright © 2019 Vibol Roeun. All rights reserved.
+//
+
+import UIKit
+
+class VierificationViewController: UIViewController {
+    
+
+    @IBOutlet var verificationText: [UIView]!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        
+    }
+    
+    func setupUI() {
+    
+        for view in verificationText {
+            view.layer.cornerRadius = view.frame.width / 2
+        }
+    
+    }
+
+    @IBAction func resentButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
